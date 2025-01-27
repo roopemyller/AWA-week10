f61bcabb0b821a2695429add674defb3b424c179
roopemyller <roope.myller@gmail.com>
2025-01-27 20:39:39 +0200

    task2

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,30 @@
+import React, {useState, useEffect} from 'react';
+
+interface Post {
+    id: number;
+    title: string;
+    body: string;
+}
 
 function About() {
 
+    const [item, setItems] = useState<Post[]>([])  
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(data => setItems(data))
+        .catch(error => console.log("Error fetching data", error))
+    }, [])
+
     return (
         <div className="about">
-            <p>this is about lol</p>
+            {item.map((item) => (
+                <div key={item.id}>
+                    <h3>{item.title}</h3>
+                    <p>{item.body}</p>
+                </div>
+            ))}
         </div>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-
 import '../styles/header.css'
 
 function Header() {
